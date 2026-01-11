5df3583fde9c3e2252a5506af8c47dd0df102914
neilw26 <neilw26@gmail.com>
2026-01-10 19:49:56 -0500

    Changes

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,33 @@
 export const projects = [
   {
-    title: "Project One",
-    image: "/project1.png",
-    date: "Jan 2025",
-    description: "A modern web app built with React and Node.js.",
-    link: "https://yourprojectlink.com",
+    title: "AI-Driven Clinical Question Answering RAG model",
+    image: "/project5.png",
+    date: "October 2025 - November 2025",
+    description: "Built a retrieval-augmented generation (RAG) pipeline in python that uses vector databases to embed and retrieve FHIR patient data, enabling an AI model to accurately answer clinical questions by overcoming LLM context limits while supporting structured healthcare analysis(Repository is private due it being an academic project)"
   },
   {
-    title: "Project Two",
+    title: "Savor Recipe Recommender with RAG",
+    image: "/project4.png",
+    date: "October 2024 - December 2024",
+    description: "Built a retrieval-augmented generation (RAG) pipeline and web application to recommend recipes based on ingredients available to users to save money, time, and effort(Repository is private due it being an academic project)"
+  },
+{
+    title: "FHIR Compliant Bed Tracking System",
+    image: "/project3.png",
+    date: "October 2025 - December 2025",
+    description: "FHIR compliant bed tracking web application with mobile friendly UI for immediate use by healthcare facilities. The application uses React, node.js, and Firebase(FHIR Compliant data schema and structure).(Repository is private due it being an academic project)"
+  },
+  {
+    title: "Atomic Object Custom Visualization Dashboard",
+    image: "/project1.png",
+    date: "September 2022 - December 2022",
+    description: "Full Stack Development to create a custom visualization dashboard that is used in every atomic object office. The dashboard uses React, Bootstrap, Javascript, and Firebase.",
+    link: "https://capstone.cse.msu.edu/2022-08/projects/atomic-object/",
+  },
+  {
+    title: "Job Offer Comparison Compare Android App",
     image: "/project2.png",
-    date: "Oct 2024",
-    description: "Mobile-first portfolio site with smooth animations.",
-    link: "https://github.com/yourrepo",
+    date: "May 2024 - August 2024",
+    description: "An Android App that allows users to compare job offers based on various factors such as salary, location, benefits, and more. Built using kotlin and Android Studio.(Repository is private due it being an academic project)"
   },
 ];
